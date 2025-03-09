7d907fa4c48ee88d517e429284c8be10383bf7d9
zach <zacharythunt@gmail.com>
2025-03-09 17:36:30 -0400

    updating test file to use Sequential API with input layer

--- testing.py
+++ testing.py
@@ -5,9 +5,10 @@ import tensorflow as tf
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 # use input layer
-inputs = tf.keras.Input(shape=(28, 28))
-x = tf.keras.layers.Flatten()(inputs)
-x = tf.keras.layers.Dense(128, activation='relu')(x)
-x = tf.keras.layers.Dropout(0.2)(x)
-outputs = tf.keras.layers.Dense(10)(x)
-model = tf.keras.Model(inputs=inputs, outputs=outputs)
+model = tf.keras.Sequential([
+    tf.keras.Input(shape=(28, 28)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10)
+])
